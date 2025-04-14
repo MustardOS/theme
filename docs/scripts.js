f288681ad6058aeddbc6a8eae94d6f7c2adfc35d
fizwidget <fizwidget@gmail.com>
2025-04-14 12:44:23 +1000

    Improved filter UI (#54)
    * Improve filter UI, so you can select a device then apply other filters
    
    * Make filters wrap nicely on narrow windows / mobile devices

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,55 +1,100 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const filterButtons = document.querySelectorAll('.filter-btn');
+    // UI elements
+    const deviceFilterMenu = document.getElementById('device-filter');
+    const hdmiFilterCheckbox = document.getElementById('hdmi-filter');
+    const gridFilterCheckbox = document.getElementById('grid-filter');
+    const languageFilterCheckbox = document.getElementById('language-filter');
     const portfolioItems = document.querySelectorAll('.portfolio-item');
-    
-    // Primary colors corresponding to each filter category
-    const filterColors = {
-        'all': '#c7af26',
-        'RG28XX': '#ba5a31',
-        'RG34XX': '#7cafc4',
-        'RG35XX': '#243e36',
-        'RG40XX': '#9e778f',
-        'RGCUBEXX': '#870058',
-        'TrimUIBrick': '#F25F5C',
-        'TrimUISmartPro': '#243e36',
-        'Legacy': '#c7af26'
-    };
 
-    // Updates the UI to reflect the filter category set in the URL
+    // Updates the UI to reflect the URL query params
     function updateUI() {
-        // Get filter category from URL
-        const urlParams = new URLSearchParams(window.location.search);
-        const filterCategory = urlParams.get('filter') ?? 'all';
+        // Get filters from query params
+        const deviceFilter = getQueryParm(queryParams.device) ?? 'all';
+        const isHdmiFilterEnabled = getQueryParm(queryParams.hdmi) === 'true';
+        const isGridFilterEnabled = getQueryParm(queryParams.grid) === 'true';
+        const isLanguageFilterEnabled = getQueryParm(queryParams.language) === 'true';
 
-        // Reset theme colors
-        const color = filterColors[filterCategory];
+        // Update filter UI
+        deviceFilterMenu.value = deviceFilter;
+        hdmiFilterCheckbox.checked = isHdmiFilterEnabled;
+        gridFilterCheckbox.checked = isGridFilterEnabled;
+        languageFilterCheckbox.checked = isLanguageFilterEnabled;
+
+        // Update page color scheme
+        const color = primaryColors[deviceFilter];
         document.documentElement.style.setProperty('--primary-color', color);
 
         // Show or hide portfolio items
         portfolioItems.forEach(item => {
-            const categories = item.getAttribute('data-category').split(' '); // Get the categories as an array
-            const parentItem = item.closest('a'); // Find the parent <a> element
-            const isVisible = filterCategory === 'all' || categories.includes(filterCategory);
-            parentItem.style.display = isVisible ? 'block' : 'none';
-        });
-
-        // Update filter button styles
-        filterButtons.forEach(button => {
-            const buttonFilter = button.getAttribute('data-filter');
-            button.classList.toggle('applied', buttonFilter === filterCategory);
+            // E.g. "RG35XX RG40XX HDMI Grid"
+            const categories = item.getAttribute('data-category').split(' ');
+            
+            const isMatchingDeviceFilter = deviceFilter === 'all' || categories.includes(deviceFilter);
+            const isMatchingHdmiFilter = !isHdmiFilterEnabled || categories.includes('HDMI');
+            const isMatchingGridFilter = !isGridFilterEnabled || categories.includes('Grid');
+            const isMatchingLanguageFilter = !isLanguageFilterEnabled || categories.includes('Language');
+            
+            const isMatchingAllFilters =
+                isMatchingDeviceFilter &&
+                isMatchingHdmiFilter &&
+                isMatchingGridFilter &&
+                isMatchingLanguageFilter;
+            
+            // Only show items that match all filters
+            const parentItem = item.closest('a');
+            parentItem.style.display = isMatchingAllFilters ? 'block' : 'none';
         });
     }
 
-    // Update URL + UI when button is clicked
-    filterButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const url = new URL(window.location);
-            url.searchParams.set("filter", this.getAttribute('data-filter'));
-            window.history.replaceState(null, '', url);
-            updateUI();
-        });
+    // Update URL & UI when filters are changed
+    deviceFilterMenu.addEventListener('change', function() {
+        setQueryParam(queryParams.device, this.value);
+        updateUI();
     });
-    
+    hdmiFilterCheckbox.addEventListener('change', function() {
+        setQueryParam(queryParams.hdmi, this.checked);
+        updateUI();
+    });
+    gridFilterCheckbox.addEventListener('change', function() {
+        setQueryParam(queryParams.grid, this.checked);
+        updateUI();
+    });
+    languageFilterCheckbox.addEventListener('change', function() {
+        setQueryParam(queryParams.language, this.checked);
+        updateUI();
+    });
+
     // Update UI on initial page load
     updateUI();
 });
+
+const getQueryParm = (key) =>
+    new URLSearchParams(window.location.search).get(key);
+
+const setQueryParam = (key, value) => {
+    const params = new URLSearchParams(window.location.search);
+    if (value) {
+        params.set(key, value);
+    } else {
+        params.delete(key);
+    }
+    window.history.replaceState({}, '', `${window.location.pathname}?${params}`);
+}
+
+const queryParams = {
+    device: 'device',
+    hdmi: 'hdmi',
+    grid: 'grid',
+    language: 'language'
+}
+
+const primaryColors = {
+    all: '#c7af26',
+    RG28XX: '#ba5a31',
+    RG34XX: '#7cafc4',
+    RG35XX: '#243e36',
+    RG40XX: '#9e778f',
+    RGCUBEXX: '#870058',
+    TrimUIBrick: '#F25F5C',
+    TrimUISmartPro: '#243e36',
+};
